0cf00e9c0e6543107f031a589ef3dd84232e6e52
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-07 19:08:01 +0700

    [CustomerRestcontroller.java] : remove used import and other ;

--- src/main/java/com/sattaporn/controller/CustomerRestcontroller.java
+++ src/main/java/com/sattaporn/controller/CustomerRestcontroller.java
@@ -1,9 +1,5 @@
 package com.sattaporn.controller;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.sattaporn.dto.CustomerDTO;
 import com.sattaporn.model.Customer;
 import com.sattaporn.service.CustomerService;
-
-import javassist.bytecode.ByteArray;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
@@ -54,7 +47,6 @@ public class CustomerRestcontroller {
 		return new ResponseEntity<Customer>(updatedCustomer,HttpStatus.OK);
 	}
 	
-//	@CrossOrigin("http://localhost:4200")
 	@RequestMapping(path = "/remove/{id}" , method = RequestMethod.DELETE)
 	public ResponseEntity<?> removeCustomer(@PathVariable int id){
 		System.out.println("[PONDINGS] Start method removeCustomer");
@@ -100,7 +92,6 @@ public class CustomerRestcontroller {
 	
 	@RequestMapping(path = "/downloadDocument/{code}" , method = RequestMethod.GET)
 	public ResponseEntity<byte[]> downloadDocument(@PathVariable String code) {
-		System.out.println("asd = " + code);
 		CustomerDTO customerDTO = new CustomerDTO();
 		customerDTO.setSearchKeyword(code);
 		customerDTO.setFindMethod("code");
