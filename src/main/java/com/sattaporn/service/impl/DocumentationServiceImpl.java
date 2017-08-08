5985705efcbb9cbe37ec9bd6fec2da0b932bded0
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-08 21:11:32 +0700

    [Service Implement] (DocumentationServiceImpl.java) : Add documentation service implement ;

---
+++ src/main/java/com/sattaporn/service/impl/DocumentationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sattaporn.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sattaporn.service.DocumentationService;
+
+@Service
+@Transactional
+public class DocumentationServiceImpl implements DocumentationService{
+
+}
