5f9a2235b69bc971f870e3205b5d3b425b5c2c65
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-09 14:07:55 +0700

    [Service-Implement] (DocumentationServiceImpl.java) : Implement all new method from document service ;

--- src/main/java/com/sattaporn/service/impl/DocumentationServiceImpl.java
+++ src/main/java/com/sattaporn/service/impl/DocumentationServiceImpl.java
@@ -1,12 +1,44 @@
 package com.sattaporn.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sattaporn.model.Documentation;
+import com.sattaporn.repository.DocumentationRepository;
 import com.sattaporn.service.DocumentationService;
 
 @Service
 @Transactional
 public class DocumentationServiceImpl implements DocumentationService{
+
+	@Autowired
+	DocumentationRepository documentRepository;
+	
+	@Override
+	public Documentation createDocument(Documentation documentation) {
+		Documentation createdDocument = documentRepository.save(documentation);
+		createdDocument.setCode("DOCX"+String.format("%05d", createdDocument.getId()));
+		Documentation updatedDocumentcode = documentRepository.save(createdDocument);
+		return updatedDocumentcode;
+	}
+
+	@Override
+	public Documentation findDocument(Documentation documentation) {
+		
+		return null;
+	}
+
+	@Override
+	public Documentation updateDocument(Documentation documentation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeDocument(int id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
