83e8997e9418d68993ada3bceb69139a2c1b8a8d
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-09 14:04:22 +0700

    [Controller] (Documentationrestcontroller.java) : Try method uploadDocument ;

--- src/main/java/com/sattaporn/controller/DocumentationRestcontroller.java
+++ src/main/java/com/sattaporn/controller/DocumentationRestcontroller.java
@@ -1,20 +1,32 @@
 package com.sattaporn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sattaporn.model.Documentation;
+import com.sattaporn.service.DocumentationService;
 
 @RestController
 @RequestMapping("ducmentation")
 public class DocumentationRestcontroller {
 
-	@RequestMapping(path="create",method=RequestMethod.POST)
-	public ResponseEntity<?> createDocument(@RequestBody Documentation documentation) {
+	@Autowired
+	DocumentationService documentService;
+	
+	@RequestMapping(path="uploadDocument",method=RequestMethod.POST)
+	public ResponseEntity<?> createDocument(@RequestParam("file") Object obj) {
+		MultipartFile file = (MultipartFile) obj;
+		System.out.println(file.getName());
+		System.out.println(file.getSize());
+		System.out.println(file.getContentType());
+		System.out.println(file.getOriginalFilename());
 		return null;
 	}
 	
