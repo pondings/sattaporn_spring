b6241541153c8f4e0249c39ceca0d8dfe9bf7a64
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-09 14:07:17 +0700

    [Service] (DocumentationService.java) Add new method for documentation ;

--- src/main/java/com/sattaporn/service/DocumentationService.java
+++ src/main/java/com/sattaporn/service/DocumentationService.java
@@ -1,5 +1,15 @@
 package com.sattaporn.service;
 
+import com.sattaporn.model.Documentation;
+
 public interface DocumentationService {
 
+	public Documentation createDocument(Documentation documentation);
+	
+	public Documentation findDocument(Documentation documentation);
+	
+	public Documentation updateDocument(Documentation documentation);
+	
+	public void removeDocument(int id) ;
+	
 }
