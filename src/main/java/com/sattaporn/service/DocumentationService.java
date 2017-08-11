4a1019f741a28edafab0181778821f9c9d8f7c79
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-11 19:27:50 +0700

    Edit interface method to receive DTO ;

--- src/main/java/com/sattaporn/service/DocumentationService.java
+++ src/main/java/com/sattaporn/service/DocumentationService.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.sattaporn.dto.DocumentationDTO;
 import com.sattaporn.model.Documentation;
 
 public interface DocumentationService {
 
 	public Documentation createDocument(MultipartFile file, String custCode);
 	
-	public List<Documentation> findDocument(Documentation documentation);
+	public List<Documentation> findDocument(DocumentationDTO documentation);
 	
 	public Documentation updateDocument(Documentation documentation);
 	
