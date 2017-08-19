5f1e8f063608ab385345eb1eb4c2e377fcf3b503
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-20 00:45:22 +0700

    Add default serailize

--- src/main/java/com/sattaporn/model/Service.java
+++ src/main/java/com/sattaporn/model/Service.java
@@ -23,6 +23,10 @@ import javax.persistence.UniqueConstraint;
 @Table(name = "service", schema = "public", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class Service implements java.io.Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int id;
 	private String name;
 	private String description;
