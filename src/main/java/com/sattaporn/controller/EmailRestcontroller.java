960e132b670c44ec90c74fd002b7a9051df75959
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-22 23:49:52 +0700

    set email with model

--- src/main/java/com/sattaporn/controller/EmailRestcontroller.java
+++ src/main/java/com/sattaporn/controller/EmailRestcontroller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.sattaporn.model.Email;
 import com.sattaporn.service.EmailService;
 
 @RestController
@@ -17,9 +18,21 @@ public class EmailRestcontroller {
 	EmailService emailservice ;
 	
 	@RequestMapping(path="send", method=RequestMethod.POST)
-	public void sendEmail(@RequestParam("files") MultipartFile[] files) {
-		System.out.println("File list size " + files.length);
-		emailservice.sendMail(files);
+	public void sendEmail(@RequestParam("files") MultipartFile[] files, @RequestParam("username") String username,
+			@RequestParam("password") String password, @RequestParam("content") String content, @RequestParam("sendFrom") String sendFrom,
+			@RequestParam("sendTo") String sendTo, @RequestParam("subject") String subject) {
+		Email email = new Email() ;
+		
+		email.setUsername(username);
+		email.setPassword(password);
+		email.setContent(content);
+		email.setSendFrom(sendFrom);
+		email.setSendTo(sendTo);
+		email.setSubject(subject);
+		
+		System.out.println("Email =  " + email.toString());
+		
+//		emailservice.sendMail(files);
 		
 	}
 	
