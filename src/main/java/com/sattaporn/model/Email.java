9f88f9cd538dcc949c6812977ee5d713879eaaec
Pawarut Klaiarmon <pawarut.klaiarmon@gmail.com>
2017-08-23 00:56:03 +0700

    Change request file size

---
+++ src/main/java/com/sattaporn/model/Email.java
@@ -0,0 +1,55 @@
+package com.sattaporn.model;
+
+public class Email {
+
+	private String username;
+	private String password;
+	private String sendFrom;
+	private String sendTo;
+	private String subject;
+	private String content;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSendFrom() {
+		return sendFrom;
+	}
+	public void setSendFrom(String sendFrom) {
+		this.sendFrom = sendFrom;
+	}
+	public String getSendTo() {
+		return sendTo;
+	}
+	public void setSendTo(String sendTo) {
+		this.sendTo = sendTo;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	@Override
+	public String toString() {
+		return "Email [username=" + username + ", password=" + password + ", sendFrom=" + sendFrom + ", sendTo="
+				+ sendTo + ", subject=" + subject + ", content=" + content + "]";
+	}
+	
+}
