4b4a49dc152ffb1fe53622ffb5c51cb6d2dfb4a4
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-22 19:21:39 +0700

    Test send mail with multiple files

--- src/main/java/com/sattaporn/controller/EmailRestcontroller.java
+++ src/main/java/com/sattaporn/controller/EmailRestcontroller.java
@@ -1,24 +1,26 @@
 package com.sattaporn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.sattaporn.service.EmailService;
+
 @RestController
 @RequestMapping("email")
 public class EmailRestcontroller {
 
+	@Autowired
+	EmailService emailservice ;
 	
 	@RequestMapping(path="send", method=RequestMethod.POST)
 	public void sendEmail(@RequestParam("files") MultipartFile[] files) {
 		System.out.println("File list size " + files.length);
+		emailservice.sendMail(files);
 		
-		
-//		for(MultipartFile file: emailDTO.getMultipartFileList()) {
-//			System.out.println("file name : " + file.getOriginalFilename());
-//		} 
 	}
 	
 	
